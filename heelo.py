065e1d006698b0b2d28ec50760f5c91a727b6061
fazal <saadatfazal@gmail.com>
2024-02-03 12:56:14 +0500

    first python code

---
+++ heelo.py
@@ -0,0 +1,8 @@
+# Simple Python Script for Greeting
+
+# Prompt the user for their name
+name = input("Enter your name: ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Welcome to the Python world.")
+
